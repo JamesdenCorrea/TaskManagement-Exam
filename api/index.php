5f0436ddf0503cbdb65f037a7ca403299d440c5a
Jamesden Correa <jamesdencorrea@Robins-iMac.local>
2026-03-11 10:10:10 +0800

    fix: point storage to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -1,2 +1,10 @@
 <?php
+define('LARAVEL_START', microtime(true));
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->useStoragePath('/tmp/storage');
+
 require __DIR__ . '/../public/index.php';
